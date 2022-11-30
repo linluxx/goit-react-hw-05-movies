5ed806819469814f561e510ce3c07627fc050c32
linluxx <karolina.kkv@gmail.com>
2022-11-30 16:24:26 +0300

    added normalize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, Global, css } from '@emotion/react';
-
+import normalize from 'normalize.css';
 import theme from 'constants/theme';
 import globalStyles from 'constants/globalStyles';
 import { App } from 'components/App';
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Global
         styles={css`
           ${globalStyles}
+          ${normalize}
         `}
       />
       <ThemeProvider theme={theme}>
